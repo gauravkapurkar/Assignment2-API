a35de180d769d850fc22da78cf11e7456df021e4
gauravkapurkar <gauravkapurkar@gmail.com>
2021-07-02 17:43:12 +0530

    added app.http for sending api requests & added documentation using swagger-jsdoc in teacher.js file

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const app = express();
- 
 const teacherRoute = require('./teacher'); 
+
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUiExpress = require('swagger-ui-express');
+
+const swaggerOptions = {
+    swaggerDefinition:{
+        info:{
+            title: 'Teacher API',
+            description: 'API for teacher registration, login, updation, deletion',
+            version: '1.0.0',
+            contact:{
+                name:'Gaurav Kapurkar'
+            },
+            servers: [{
+                url:"http://localhost:5000/",
+                description:'local dev server'
+            }]
+        }
+    },
+    apis: ["./teacher.js"]
+};
+
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerDocs));
 
 app.use(express.json());// for accepting json objects
 
--- teacher.js
+++ teacher.js
@@ -7,24 +7,136 @@ const {
     registerTeacher, 
     getAllTeacher, 
     getTeacherById
-                    } = require('./controller/mainFunctions');
+    } = require('./controller/mainFunctions');
 
-//fetching teachers 
+/**
+ * @swagger
+ * /teachers:
+ *      get:
+ *          summary: fetching teachers
+ *          description: get all existing teachers
+ *          responses:
+ *              '200':
+ *                  description: successfully get all teachers
+ */
 router.get('/', getAllTeacher)
 
-//fetching teachers by id
+/**
+ * @swagger
+ * /teachers/{id}:
+ *      get:
+ *          summary: fetching teachers by id
+ *          description: get teacher with requested id
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  required: true
+ *                  description: numeric id to retrieve teacher
+ *          responses:
+ *              '200':
+ *                  description: successfully get teacher
+ *              '400':
+ *                  description: requested teacher not exist
+ */
 router.get('/:id', getTeacherById)
 
-//creating teacher
+
+/**
+ * @swagger
+ * /teachers/register:
+ *      post:
+ *          summary: creating teacher
+ *          description: create teacher with name & password
+ *          parameters:
+ *              -   in: body
+ *                  name: nameAndPassword
+ *                  required: true
+ *                  description: enter name & password in json object format            
+ *          responses:
+ *              '201':
+ *                  description: teacher registered successfully
+ *              '400':
+ *                  description: please enter name & password as per instructions
+ *              '500':
+ *                  description: server having some issues
+ */
+
 router.post('/register', registerTeacher)
 
-//route for login by id
+
+/**
+ * @swagger
+ * /teachers/login/{id}:
+ *      post:
+ *          summary: teacher login by id
+ *          description: teacher login using id, name & password
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  required: true
+ *                  description: numeric id to retrieve teacher
+ *              -   in: body
+ *                  name: nameAndPassword
+ *                  required: true
+ *                  description: enter name & password in json object format 
+ *          responses:
+ *              '200':
+ *                  description: successfully logged in
+ *              '400':
+ *                  description: requested teacher registered
+ *              '401':
+ *                  description: invalid password
+ *              '500': 
+ *                  description: server having some issues
+ */
 router.post('/login/:id', loginTeacherById)
 
-//update teacher by id
-router.put('/edit/:id', updateTeacherById)
 
-//delete teacher
+/**
+ * @swagger
+ * /teachers/update/{id}:
+ *      put:
+ *          summary: update teacher by id
+ *          description: update teacher name & password
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  required: true
+ *                  description: numeric id to retrieve teacher
+ *              -   in: body
+ *                  name: nameAndPassword
+ *                  required: true
+ *                  description: enter name & password in json object format 
+ *          responses:
+ *              '200':
+ *                  description: teacher updated successfully 
+ *              '400':
+ *                  description: enter updated name & password as per instructions
+ *              '404':
+ *                  description: teacher not found
+ *              '500': 
+ *                  description: server having some issues
+ */
+router.put('/update/:id', updateTeacherById)
+
+
+/**
+ * @swagger
+ * /teachers/delete/{id}:
+ *      delete:
+ *          summary: delete teacher
+ *          description: delete teacher using id
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  required: true
+ *                  description: numeric id to retrieve teacher
+ *          responses:
+ *              '200':
+ *                  description: teacher deleted successfully 
+ *              '404':
+ *                  description: teacher not found
+ */
 router.delete('/delete/:id', deleteTeacher)
 
 module.exports = router;
